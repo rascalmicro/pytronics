fefadcece76995d93288861749b8a71a164dc1e3
Brandon Stafford <brandon@pingswept.org>
2011-04-20 14:03:07 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rascal',
+    version='0.1',
+    py_modules=['rascal'],
+    description='Rascal hardware API',
+    long_description='Hardware control library for the Rascal, a small computer for art and science',
+    author='Brandon Stafford',
+    author_email='brandon@rascalmicro.com',
+    url='http://rascalmicro.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python'
+        ],
+    )
