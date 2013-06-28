15aa383f27b223133f1082cc622ead42431bb1a9
Brandon Stafford <brandon@rascalmicro.com>
2013-06-28 14:04:28 -0400

    Fix version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pytronics',
-    version='0.1',
+    version='0.4.1',
     license='GPLv3',
     py_modules=['pytronics'],
     description='Rascal hardware API',
